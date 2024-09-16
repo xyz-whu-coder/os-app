4a1cb8fbaa4c9e75626531737f062995546ab4e1
Xiao Yizhao <xyz4821@163.com>
2024-09-16 14:46:14 +0800

    fix task1_1.sh for confronting with a problem while writing ex-report...

--- 02-shell/task1/task1_1.sh
+++ 02-shell/task1/task1_1.sh
@@ -2,9 +2,10 @@
 read -p "input a file:" filename
 #请添加代码，判断文件是不是块或字符设备文件
 # if [...]
-if [ -b $filename -o -c $filename ]
-	echo $filename is not char device and not block device
-then
+if [ -b $filename -o -c $filename ]; then
+	echo $filename is a char device or a block device
 	#请添加代码，将文件拷贝到/dev目录下面
 	sudo cp $filename /dev/
+else 
+	echo $filename is not char device and not block device
 fi
